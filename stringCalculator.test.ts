14a45321dd1d3aae2814046dc3be9586cfe131e8
Swapnil Akolkar <33319248+swap7206@users.noreply.github.com>
2025-03-23 17:59:13 +0000

    test: test for two comma-separated numbers

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -8,4 +8,8 @@ describe("String Calculator", () => {
   test("returns the number itself for a single number", () => {
     expect(add("1")).toBe(1);
   });
+
+  test("returns sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+  });
 });
