b7c9cb359fb5a4216a6e10a60e0613c3ef785624
Swapnil Akolkar <33319248+swap7206@users.noreply.github.com>
2025-03-23 18:07:27 +0000

    test: handles multiple numbers

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -12,4 +12,8 @@ describe("String Calculator", () => {
   test("returns sum of two numbers", () => {
     expect(add("1,5")).toBe(6);
   });
+
+  test("handles multiple numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
 });
