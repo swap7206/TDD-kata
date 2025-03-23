26327f46d2bb4174f08be6bdb5e661f112573374
Swapnil Akolkar <33319248+swap7206@users.noreply.github.com>
2025-03-23 18:18:17 +0000

    feat: added support for custom delimiter

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,5 +1,12 @@
 export function add(numbers: string): number {
     if (!numbers) return 0;
-    const numArray = numbers.split(/,|\n/).map((num) => Number(num));
+
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(parts[0].slice(2));
+      numbers = parts[1];
+    }
+    const numArray = numbers.split(delimiter).map((num) => Number(num));
     return numArray.reduce((sum, num) => (sum + num), 0);
   }
