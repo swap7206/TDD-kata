09c750d02d45862d2444ec06f04d04795f8eff16
Swapnil Akolkar <33319248+swap7206@users.noreply.github.com>
2025-03-23 18:12:17 +0000

    feat: support newline delimiter

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
     if (!numbers) return 0;
-    const numArray = numbers.split(",").map((num) => Number(num));
+    const numArray = numbers.split(/,|\n/).map((num) => Number(num));
     return numArray.reduce((sum, num) => (sum + num), 0);
   }
